3f2ed6685c1dda285b90ccfd94a8af736a1b280e
joao.teodoro <joao.teodoro1610@gmail.com>
2026-02-10 22:15:38 -0300

    Adicionando Firebase ao projeto

---
+++ lib/firebase_options.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for ios - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyAJKNBUQzbMPifE6SvZ2qJX2eBJcwQ1q1M',
+    appId: '1:486956293149:android:199f760a8ef9773ec8faf4',
+    messagingSenderId: '486956293149',
+    projectId: 'chaty-app-41520',
+    storageBucket: 'chaty-app-41520.firebasestorage.app',
+  );
+}
